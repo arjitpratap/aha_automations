264a7d42496bcb27adf0c1732dc4ae6ba3b3869a
arjitpratap <114023769+arjitpratap@users.noreply.github.com>
2025-05-12 20:36:14 +0530

    Initial Commit

---
+++ aha_automations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aha_automations
+//
+//  Created by Arjit Pratap Singh Chauhan on 12/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aha_automations/aha_automationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  aha_automationsApp.swift
+//  aha_automations
+//
+//  Created by Arjit Pratap Singh Chauhan on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct aha_automationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
